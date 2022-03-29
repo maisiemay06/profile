112d811495713b90cdd1f018aa7048d85410e397
maisiemay06 <73712593+maisiemay06@users.noreply.github.com>
2022-03-29 16:14:20 +0100

    Style In Meeow Profile card

--- src/InMeeowProfile.js
+++ src/InMeeowProfile.js
@@ -2,27 +2,36 @@ import React from "react";
 
 export default function InMeeowProfile() {
     return (
-        <div className="card in-meeow-profile">
-          <i class="fa-solid fa-pen-to-square edit-btn"></i>  
-          <h3 className="section-title">In Meeow profile</h3>
-          <p className="section-descrip"></p>
-          <div className="card in-meeow-about">
-            <h3 className="section-title">About</h3>
-            <p className="section-descrip">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nemo quibusdam officiis explicabo ut recusandae! Accusamus nesciunt, tempora quis perferendis non beatae maiores numquam delectus et sequi voluptatem eligendi animi!</p>
+      <div className="card in-meeow-profile">
+        <i class="fa-solid fa-pen-to-square edit-btn"></i> 
+        <div className="container">
+          <div className="in-meeow-head">
+            <h3 className="section-title">In Meeow profile</h3>
+            <p className="section-descrip">This info will be displayed in your rollover profile during Meeows.</p>
           </div>
-          <div className="card in-meeow-service-skills">
+          <div className="row">
+            <div className="card col in-meeow-about">
+              <h3 className="section-title">About</h3>
+              <p className="section-descrip">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nemo quibusdam officiis explicabo ut recusandae! Accusamus nesciunt, tempora quis perferendis non beatae maiores numquam delectus et sequi voluptatem eligendi animi!</p>
+            </div>
+            <div className="col-xs gap"></div>
+            <div className="card col container in-meeow-service-skills">
             <h3 className="section-title">Services/Skills</h3>
-            <ul className="services">
-              <li>Service 1</li>
-              <li>Service 2</li>
-              <li>Service 3</li>
-            </ul>
-            <ul className="skills">
-              <li>Skill 1</li>
-              <li>Skill 2</li>
-              <li>Skill 3</li>
-            </ul>
+              <div className="row">
+                <ul className="services col">
+                  <li>Service 1  X</li>
+                  <li>Service 2  X</li>
+                  <li>Service 3  X</li>
+                </ul>
+                <ul className="skills col">
+                  <li>Skill 1  X</li>
+                  <li>Skill 2  X</li>
+                  <li>Skill 3  X</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
     )
 }
