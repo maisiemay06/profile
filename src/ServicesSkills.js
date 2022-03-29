8cd00cc48a71e0865267fd0b91c29dd4d70755f4
maisiemay06 <73712593+maisiemay06@users.noreply.github.com>
2022-03-29 17:01:01 +0100

    Style Services and Skills card

--- src/ServicesSkills.js
+++ src/ServicesSkills.js
@@ -4,34 +4,37 @@ export default function ServicesSkills() {
     return (
         <div className="card services-skills">
           <i class="fa-solid fa-pen-to-square edit-btn"></i>
-          <div className="services">
-            <h3 className="service-title">Services</h3>
-            <ul>
-              <li>Service 1</li>
-              <li>Service 2</li>
-              <li>Service 3</li>
-              <li>Service 4</li>
-              <li>Service 5</li>
-              <li>Service 6</li>
-              <li>Service 7</li>
-              <li>Service 8</li>
-              <li>Service 9</li>
-            </ul>
-          </div>
-          <div className="skills">
-            <h3 className="section-title">
-              <ul>
-                <li>Skill 1</li>
-                <li>Skill 2</li>
-                <li>Skill 3</li>
-                <li>Skill 4</li>
-                <li>Skill 5</li>
-                <li>Skill 6</li>
-                <li>Skill 7</li>
-                <li>Skill 8</li>
-                <li>Skill 9</li>
-              </ul>
-            </h3>
+          <div className="container">
+            <div className="row">
+              <div className="services card col">
+                <h3 className="section-title">Services</h3>
+                <ul>
+                  <li>Service 1  X</li>
+                  <li>Service 2  X</li>
+                  <li>Service 3  X</li>
+                  <li>Service 4  X</li>
+                  <li>Service 5  X</li>
+                  <li>Service 6  X</li>
+                  <li>Service 7  X</li>
+                  <li>Service 8  X</li>
+                  <li>Service 9  X</li>
+                </ul>
+              </div>
+              <div className="skills card col">
+                <h3 className="section-title">Skills</h3>
+                  <ul>
+                    <li>Skill 1  X</li>
+                    <li>Skill 2  X</li>
+                    <li>Skill 3  X</li>
+                    <li>Skill 4  X</li>
+                    <li>Skill 5  X</li>
+                    <li>Skill 6  X</li>
+                    <li>Skill 7  X</li>
+                    <li>Skill 8  X</li>
+                    <li>Skill 9  X</li>
+                  </ul>
+              </div>
+            </div>
           </div>
         </div>
     )
